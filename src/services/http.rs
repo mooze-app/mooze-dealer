ce9aa1a96913c56fcbc793ce90f5c77a4aa17c36
h4vismat <h4vismat@pm.me>
2025-03-24 09:41:02 -0300

    New method to handle status updates from Eulen.

--- src/services/http.rs
+++ src/services/http.rs
@@ -10,15 +10,16 @@ use serde_json::json;
 use tokio::sync::{mpsc, oneshot};
 use tower_http::trace::TraceLayer;
 
-use super::{transactions::TransactionServiceRequest, ServiceError};
+use super::{pix::PixServiceRequest, transactions::TransactionServiceRequest, ServiceError};
 use crate::models::{
-    self,
+    self, pix,
     transactions::{NewTransaction, Transaction},
 };
 
 #[derive(Clone)]
 struct AppState {
     transaction_channel: mpsc::Sender<TransactionServiceRequest>,
+    pix_channel: mpsc::Sender<PixServiceRequest>,
 }
 
 #[derive(Serialize)]
@@ -73,21 +74,61 @@ async fn request_new_deposit(
     }
 }
 
+async fn eulen_update_status(
+    State(state): State<AppState>,
+    Json(req): Json<pix::EulenDepositStatus>,
+) -> impl IntoResponse {
+    let (pix_tx, pix_rx) = oneshot::channel();
+    let pix_result = state
+        .pix_channel
+        .send(PixServiceRequest::UpdateEulenStatus {
+            eulen_status: req,
+            response: pix_tx,
+        })
+        .await;
+
+    if let Err(e) = pix_result {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"description": format!("Failed to process request: {}", e)})),
+        );
+    };
+
+    match pix_rx.await {
+        Ok(Ok(update)) => (
+            StatusCode::OK,
+            Json(json!({"description": "Status updated successfully"})),
+        ),
+        Ok(Err(service_error)) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"description": format!("Internal server error.")})),
+        ),
+        Err(e) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!({"description": format!("Failed to receive response: {}", e)})),
+        ),
+    }
+}
+
 pub async fn start_http_server(
     transaction_channel: mpsc::Sender<TransactionServiceRequest>,
+    pix_channel: mpsc::Sender<PixServiceRequest>,
 ) -> Result<(), anyhow::Error> {
     let app_state = AppState {
         transaction_channel,
+        pix_channel,
     };
 
     let app = Router::new()
         .route("/deposit", post(request_new_deposit))
+        .route("/eulen_update_status", post(eulen_update_status))
+        .route("/hello", get(|| async { "Hello, World!" }))
         .route("/health", get(|| async { "OK" }))
         .with_state(app_state)
         .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
-    println!("Listening on {}", listener.local_addr()?);
+    println!("[INFO] Listening on {}", listener.local_addr()?);
 
     axum::serve(listener, app).await?;
 
