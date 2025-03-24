1674b882f5399a0933e1956d0317298ac0e3526d
h4vismat <h4vismat@pm.me>
2025-03-24 03:10:17 -0300

    Created function to start services.

--- src/services.rs
+++ src/services.rs
@@ -1,14 +1,22 @@
 use async_trait::async_trait;
+use liquid::LiquidRequest;
+use sqlx::PgPool;
 use tokio::sync::mpsc;
 
-mod blockchain;
+use crate::settings::Settings;
+
 mod database;
+mod http;
 mod liquid;
 mod pix;
 mod transactions;
 
 #[derive(Debug, thiserror::Error)]
 enum ServiceError {
+    #[error("Internal error: {0}")]
+    Internal(String),
+    #[error("Database error: {0}")]
+    Database(String),
     #[error("Repository error: {0} - {0}")]
     Repository(String, String),
     #[error("Communication error: {0} - {1}")]
@@ -42,4 +50,51 @@ where
     }
 }
 
-pub struct ServiceManager {}
+async fn start_services(pool: PgPool, settings: Settings) -> Result<(), anyhow::Error> {
+    let (transaction_tx, mut transaction_rx) = mpsc::channel(512);
+    let (liquid_tx, mut liquid_rx) = mpsc::channel(512);
+    let (pix_tx, mut pix_rx) = mpsc::channel(512);
+
+    let mut transaction_service = transactions::TransactionService::new();
+    let mut liquid_service = liquid::LiquidService::new();
+    let mut pix_service = pix::PixService::new();
+
+    transaction_service
+        .run(
+            transactions::TransactionRequestHandler::new(
+                pool.clone(),
+                liquid_tx.clone(),
+                pix_tx.clone(),
+            ),
+            &mut transaction_rx,
+        )
+        .await;
+
+    liquid_service
+        .run(
+            liquid::LiquidRequestHandler::new(
+                settings.wallet.mnemonic,
+                settings.electrum.url,
+                settings.wallet.wallet_dir,
+                false,
+            ),
+            &mut liquid_rx,
+        )
+        .await;
+
+    pix_service
+        .run(
+            pix::PixRequestHandler::new(
+                settings.depix.auth_token,
+                settings.depix.url,
+                pool,
+                transaction_tx.clone(),
+            ),
+            &mut pix_rx,
+        )
+        .await;
+
+    http::start_http_server(transaction_tx.clone()).await?;
+
+    Ok(())
+}
