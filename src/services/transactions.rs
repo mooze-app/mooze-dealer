4ecbfa6ec64d60b8045fc002e90b931ec242f093
h4vismat <h4vismat@pm.me>
2025-03-24 16:22:11 -0300

    Add method to collect fees.

--- src/services/transactions.rs
+++ src/services/transactions.rs
@@ -1,10 +1,11 @@
 use super::liquid::LiquidRequest;
 use super::pix::PixServiceRequest;
 use crate::models::pix::Deposit;
-use crate::models::transactions::*;
+use crate::models::transactions;
 use crate::repositories::transactions::TransactionRepository;
 use async_trait::async_trait;
-use axum::{http::StatusCode, Json};
+use futures_util::TryFutureExt;
+use lwk_wollet::elements::pset::PartiallySignedTransaction;
 use sqlx::PgPool;
 use tokio::sync::{mpsc, oneshot};
 
@@ -146,7 +147,7 @@ impl TransactionRequestHandler {
         transaction_id: &String,
         status: &String,
     ) -> Result<String, ServiceError> {
-        let transaction_id = self
+        let _ = self
             .repository
             .update_transaction_status(transaction_id, status)
             .await
@@ -154,10 +155,98 @@ impl TransactionRequestHandler {
                 ServiceError::Repository("TransactionService".to_string(), e.to_string())
             })?;
 
-        let transaction_id_clone = transaction_id.clone();
-        if status == "eulen_depix_sent" {}
+        if status == "eulen_depix_sent" {
+            let transaction = self
+                .repository
+                .get_transaction(&transaction_id)
+                .await
+                .map_err(|e| {
+                    ServiceError::Internal(format!(
+                        "Could not retrieve transaction: {}.",
+                        e.to_string()
+                    ))
+                })?;
 
-        Ok(transaction_id)
+            match transaction {
+                None => {
+                    return Err(ServiceError::Database(format!(
+                        "Transaction not found: {}.",
+                        transaction_id
+                    )))
+                }
+                Some(transaction) => {
+                    self.continue_with_transaction(transaction).await;
+                }
+            }
+        }
+
+        Ok(transaction_id.clone())
+    }
+
+    async fn continue_with_transaction(
+        &self,
+        transaction: transactions::Transaction,
+    ) -> Result<(), ServiceError> {
+        if transaction.asset == transactions::Assets::DEPIX.hex() {
+            let asset_amount = transaction.amount_in_cents * 10_i32.pow(8); // adjust precision
+            let pset = self
+                .collect_fees_and_build_transaction(transaction, asset_amount)
+                .await?;
+        } else {
+            self.send_to_swap(transaction).await;
+        }
+
+        Ok(())
+    }
+
+    fn calculate_fees(&self, amount_in_cents: i32) -> i32 {
+        if amount_in_cents < 60 * 100 {
+            return 2 * 100;
+        } else if amount_in_cents < 500 {
+            return (amount_in_cents * 350) / 10000;
+        } else if amount_in_cents < 5000 {
+            return (amount_in_cents * 325) / 10000;
+        } else {
+            return (amount_in_cents * 275) / 10000;
+        }
+    }
+
+    async fn collect_fees_and_build_transaction(
+        &self,
+        transaction: transactions::Transaction,
+        asset_amount: i32,
+    ) -> Result<PartiallySignedTransaction, ServiceError> {
+        let fees = self.calculate_fees(transaction.amount_in_cents);
+        let net_amount_in_cents = transaction.amount_in_cents - fees;
+
+        let amount_to_send = (asset_amount * net_amount_in_cents) / transaction.amount_in_cents;
+        let asset_fee_amount = asset_amount - amount_to_send;
+
+        let (liquid_tx, liquid_rx) = oneshot::channel();
+        self.liquid_channel
+            .send(LiquidRequest::BuildTransaction {
+                address: transaction.address,
+                amount: amount_to_send,
+                asset: transaction.asset,
+                response: liquid_tx,
+            })
+            .await
+            .map_err(|e| {
+                ServiceError::Communication("Transaction => Liquid".to_string(), e.to_string())
+            })?;
+
+        let pset = liquid_rx
+            .await
+            .map_err(|e| {
+                ServiceError::Communication("Liquid => Transaction".to_string(), e.to_string())
+            })?
+            .map_err(|e| ServiceError::Repository("Liquid".to_string(), e.to_string()))?;
+
+        Ok(pset)
+    }
+
+    async fn send_to_swap(&self, transaction: transactions::Transaction) {
+        todo!();
     }
 }
 
