188abbf7f6e71aba7b863be2da47e1526dda2a16
h4vismat <h4vismat@pm.me>
2025-03-24 16:23:49 -0300

    Add asset enum.

--- src/models/transactions.rs
+++ src/models/transactions.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
-use sqlx::postgres::types;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct Transaction {
     pub id: String,
     pub user_id: String,
@@ -20,4 +19,26 @@ pub struct NewTransaction {
     pub amount_in_cents: i32,
     pub asset: String,
     pub network: String,
+}
+
+pub enum Assets {
+    DEPIX,
+    USDT,
+    LBTC,
+}
+
+impl Assets {
+    pub fn hex(&self) -> String {
+        match self {
+            Assets::DEPIX => {
+                "02f22f8d9c76ab41661a2729e4752e2c5d1a263012141b86ea98af5472df5189".to_string()
+            }
+            Assets::USDT => {
+                "ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2".to_string()
+            }
+            Assets::LBTC => {
+                "6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d".to_string()
+            }
+        }
+    }
 }
