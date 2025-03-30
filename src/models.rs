9fa1dfa27ec96ccc73d72c55f5fa7d3267380efb
h4vismat <h4vismat@pm.me>
2025-03-29 21:41:22 -0300

    New domains.

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
 pub mod pix;
 pub mod referrals;
 pub mod server;
+pub mod sideswap;
 pub mod transactions;
 pub mod users;
--- src/repositories.rs
+++ src/repositories.rs
@@ -1,4 +1,7 @@
 pub mod liquid;
 pub mod pix;
+pub mod price;
+pub mod sideswap;
+pub mod swap;
 pub mod transactions;
 pub mod users;
