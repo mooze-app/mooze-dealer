da3ec810022d559aee6cb1dc821fe53a992f9fbb
h4vismat <h4vismat@pm.me>
2025-03-29 21:42:15 -0300

    Add logging.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use log::{debug, error, info, warn};
+use log4rs;
 use sqlx::postgres::PgPoolOptions;
+use std::fs;
+use std::path::Path;
 
 mod models;
 mod repositories;
@@ -8,14 +12,24 @@ pub mod utils;
 
 #[tokio::main]
 async fn main() {
+    init_logging().unwrap(); // should not fail
+
+    info!("Starting Mooze dealer service.");
+    debug!("Loading configuration");
+
     let config = settings::Settings::new().expect("Could not load config file.");
+
+    info!(
+        "Connecting to PostgreSQL database at {}",
+        &config.postgres.url
+    );
     let conn = PgPoolOptions::new()
         .max_connections(5)
         .connect(&config.postgres.url)
         .await
         .expect("Could not connect to database.");
 
-    println!("[*] Starting services.");
+    info!("Starting services.");
     services::start_services(conn, config)
         .await
         .expect("Could not start services.");
@@ -23,5 +37,24 @@ async fn main() {
     tokio::signal::ctrl_c()
         .await
         .expect("Failed to listen for Ctrl-C");
-    println!("\n[*] Shutdown signal received, terminating.");
+    info!("\n[*] Shutdown signal received, terminating.");
+
+    info!("Service shutting down");
+}
+
+fn init_logging() -> Result<(), anyhow::Error> {
+    if !Path::new("logs").exists() {
+        fs::create_dir("logs")?;
+    }
+
+    match log4rs::init_file("log4rs.yaml", Default::default()) {
+        Ok(_) => {
+            println!("[*] Logging initialized successfully.");
+            Ok(())
+        }
+        Err(e) => {
+            println!("[ERROR] Failed to initialize logging: {}", e);
+            Err(anyhow::anyhow!("Could not initialize logging: {}", e))
+        }
+    }
 }
